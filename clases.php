fb6eb6830272f172cc6357bee64e8f46c86484f5
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-10-06 11:21:31 -0500

    agregando metodo agregarRcursos

--- clases.php
+++ clases.php
@@ -1,38 +1,50 @@
 <?php
-class RecursoBiblioteca {
-    public $id;
-    public $titulo;
-    public $autor;
-    public $anioPublicacion;
-    public $estado;
-    public $fechaAdquisicion;
-    public $tipo;
+    class RecursoBiblioteca {
+        public $id;
+        public $titulo;
+        public $autor;
+        public $anioPublicacion;
+        public $estado;
+        public $fechaAdquisicion;
+        public $tipo;
 
-    public function __construct($datos) {
-        foreach ($datos as $key => $value) {
-            if (property_exists($this, $key)) {
-                $this->$key = $value;
+        public function __construct($datos) {
+            foreach ($datos as $key => $value) {
+                if (property_exists($this, $key)) {
+                    $this->$key = $value;
+                }
             }
         }
     }
-}
 
-// Implementar las clases Libro, Revista y DVD aquí
+    // Implementar las clases Libro, Revista y DVD aquí
 
-class GestorBiblioteca {
-    private $recursos = [];
+    class GestorBiblioteca {
+        private $recursos = [];
 
-    public function cargarRecursos() {
-        $json = file_get_contents('biblioteca.json');
-        $data = json_decode($json, true);
-        
-        foreach ($data as $recursoData) {
-            $recurso = new RecursoBiblioteca($recursoData);
-            $this->recursos[] = $recurso;
+        public function cargarRecursos() {
+            $json = file_get_contents('biblioteca.json');
+            $data = json_decode($json, true);
+            
+            foreach ($data as $recursoData) {
+                $recurso = new RecursoBiblioteca($recursoData);
+                $this->recursos[] = $recurso;
+            }
+            
+            return $this->recursos;
         }
-        
-        return $this->recursos;
-    }
 
-    // Implementar los demás métodos aquí
-}
+        // Implementar los demás métodos aquí
+
+        agregarRecursos($jskjaksjalsjld){
+            
+        }
+
+        
+
+
+    }
+    
+?>
+
+

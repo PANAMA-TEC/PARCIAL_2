c44e3f3822da59d3e372835873cd86db195a4956
unknown <javett.pineda3@gmail.com>
2024-10-06 13:04:14 -0500

    agregando actualizar estado recurso

--- clases.php
+++ clases.php
@@ -56,6 +56,10 @@
             
         }
 
+        public function actualizarEstadoRecurso ($id, $nuevoEstado){
+            
+        }
+
         public function eliminarRecurso($id){
 
         }
